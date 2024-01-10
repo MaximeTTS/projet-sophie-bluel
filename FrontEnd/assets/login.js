32bc115aa5c73f95926577d07c6a7c8b87a5094d
MaximeTTS <maxime.s.turquet@gmail.com>
2024-01-10 21:03:00 +0100

    Addition of buttons in online mode with the appearance of the top-bar

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -33,21 +33,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Mise à jour du lien de connexion/déconnexion
   const loginLink = document.querySelector('nav ul li a[href="login.html"]')
+  const topBar = document.querySelector('.top__bar__css')
+  const adminElements = document.querySelectorAll('.only-admin')
+  const bodyStyle = document.body.style
   const isLoggedIn = localStorage.getItem('isLoggedIn')
 
-  if (isLoggedIn) {
-    loginLink.textContent = 'Logout'
-    loginLink.href = '#'
-    loginLink.addEventListener('click', function (e) {
-      e.preventDefault()
-      localStorage.removeItem('userToken')
-      localStorage.removeItem('isLoggedIn')
-      window.location.href = 'login.html'
-    })
-
-    // Ajoutez ici la logique pour afficher les boutons d'action si nécessaire
-  } else {
-    loginLink.textContent = 'Login'
-    loginLink.href = 'login.html'
+  function adjustTopBarAndAdminElements() {
+    if (isLoggedIn) {
+      topBar.style.display = 'flex' // Affiche la top-bar
+      adminElements.forEach((el) => (el.style.display = 'block')) // Affiche les éléments admin
+      bodyStyle.paddingTop = '60px' // Ajuste le padding du corps
+      loginLink.textContent = 'Logout'
+      loginLink.href = '#'
+      loginLink.addEventListener('click', function (e) {
+        e.preventDefault()
+        localStorage.removeItem('userToken')
+        localStorage.removeItem('isLoggedIn')
+        window.location.href = 'login.html'
+      })
+    } else {
+      topBar.style.display = 'none' // Cache la top-bar
+      adminElements.forEach((el) => (el.style.display = 'none')) // Cache les éléments admin
+      bodyStyle.paddingTop = '0' // Ajuste le padding du corps
+      loginLink.textContent = 'Login'
+      loginLink.href = 'login.html'
+    }
   }
+  adjustTopBarAndAdminElements()
 })
